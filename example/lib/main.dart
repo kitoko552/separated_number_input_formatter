3480cffc79e9cb406fbba73c5c508dad06fc767c
Kosuke Kito <blueberryjam09@gmail.com>
2019-09-27 17:52:43 +0900

    Add base of the example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(ExampleApp());
 
@@ -18,13 +19,77 @@ class ExampleApp extends StatelessWidget {
 class ExamplePage extends StatelessWidget {
   const ExamplePage();
 
+  static const textFieldTitleStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 12.0,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Example'),
       ),
-      body: const SizedBox(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'TextField with the formatter',
+              style: textFieldTitleStyle,
+            ),
+            TextField(
+              maxLines: 1,
+              inputFormatters: [
+//                SeparatedNumberInputFormatter(4),
+              ],
+            ),
+            const SizedBox(height: 24.0),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: const [
+                Expanded(
+                  child: Text(
+                    'digits',
+                    style: textFieldTitleStyle,
+                  ),
+                ),
+                SizedBox(width: 16.0),
+                Expanded(
+                  child: Text(
+                    'separator\n(default is a space)',
+                    style: textFieldTitleStyle,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    maxLines: 1,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      WhitelistingTextInputFormatter.digitsOnly,
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 16.0),
+                Expanded(
+                  child: TextField(
+                    maxLines: 1,
+                    inputFormatters: [
+                      BlacklistingTextInputFormatter(RegExp(r'\d+')),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/separated_number_input_formatter.dart
+++ lib/separated_number_input_formatter.dart
@@ -1,3 +1,5 @@
 library separated_number_input_formatter;
 
+export 'package:separated_number_input_formatter/separated_number_input_formatter.dart';
+
 export 'src/separated_number_input_formatter.dart';
